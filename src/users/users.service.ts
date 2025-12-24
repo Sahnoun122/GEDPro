013eb8327a04b1e58f5995c8914d7b60c86d0f09
sahnoun122 <khadijasahnoun70@gmail.com>
2025-12-24 14:46:34 +0100

    feat: ajouter le serverce et la validations des champs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository, Unique } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @Inject('USER_REPOSITORY')
+        private userRepo : Repository<User>,
+    ) {}
+
+    findByEmail(email : string){
+        return this.userRepo.findOne({ where: { email } });
+}
+
+  create( user : Partial<User>){
+    return this.userRepo.save(user)
+  }
+}
